d6a82a37f7fcb4f1dc3c9d495b7a9d0e3a23f468
Michael Quarne <mikedq1225@gmail.com>
2020-05-19 13:24:43 -0700

    added remote json file

--- src/redux/ducks/products/index.js
+++ src/redux/ducks/products/index.js
@@ -9,57 +9,61 @@ const SET_COUNT = "products/SET_COUNT"
 
 // 3. initial state
 const initialState = {
-  products: [],
-  count: 0
+	products: [],
+	count: 0,
 }
 
 // 4. reducer
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case GET_PRODUCTS:
-      return { ...state, products: action.payload }
-    case SET_COUNT:
-      return { ...state, count: action.payload }
-    default:
-      return state
-  }
+	switch (action.type) {
+		case GET_PRODUCTS:
+			return { ...state, products: action.payload }
+		case SET_COUNT:
+			return { ...state, count: action.payload }
+		default:
+			return state
+	}
 }
 
 // 5. action creators
 //display all pruducts from database
 function getProducts() {
-  return dispatch => {
-    axios.get("/products").then(resp => {
-      dispatch(getCount())
-      dispatch({
-        type: GET_PRODUCTS,
-        payload: resp.data
-      })
-    })
-  }
+	return (dispatch) => {
+		axios
+			.get("https://api.jsonbin.io/b/5ec43ffdf7c0f67062cf6fef")
+			.then((resp) => {
+				dispatch(getCount())
+				dispatch({
+					type: GET_PRODUCTS,
+					payload: resp.data.products,
+				})
+			})
+	}
 }
 
 // set the count of items in database
 function getCount() {
-  return dispatch => {
-    axios.get("/products").then(resp => {
-      dispatch({
-        type: SET_COUNT,
-        payload: resp.data.length
-      })
-    })
-  }
+	return (dispatch) => {
+		axios
+			.get("https://api.jsonbin.io/b/5ec43ffdf7c0f67062cf6fef")
+			.then((resp) => {
+				dispatch({
+					type: SET_COUNT,
+					payload: resp.data.products.length,
+				})
+			})
+	}
 }
 
 // 6. custom hook
 export function useProducts() {
-  const dispatch = useDispatch()
-  const products = useSelector(appState => appState.productState.products)
-  const count = useSelector(appState => appState.productState.count)
+	const dispatch = useDispatch()
+	const products = useSelector((appState) => appState.productState.products)
+	const count = useSelector((appState) => appState.productState.count)
 
-  useEffect(() => {
-    dispatch(getProducts())
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(getProducts())
+	}, [dispatch])
 
-  return { products, count }
+	return { products, count }
 }
